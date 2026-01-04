b8fd0e18af9df47a9220aad79efe03ebc951a722
Krina Khunt <krinakhunt12@gmail.com>
2026-01-04 13:41:24 +0530

    allow domain

--- Backend/main.py
+++ Backend/main.py
@@ -14,7 +14,7 @@ app = FastAPI(title="PDF Tools API")
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173"],
+    allow_origins=["http://localhost:5173", "http://localhost:3000", "http://127.0.0.1:5173", "https://pdf-merge-livid.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
